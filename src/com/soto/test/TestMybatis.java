4f472a3047090013e9f2fdbac4e71de48fc28fcf
sotowang <sotowang@qq.com>
2018-07-12 22:16:12 +0800

    CRUD

--- src/com/soto/test/TestMybatis.java
+++ src/com/soto/test/TestMybatis.java
@@ -19,11 +19,37 @@ public class TestMybatis {
 
 //        然后再根据sqlSessionFactory 得到session
         SqlSession session=sqlSessionFactory.openSession();
+        //增
+//        Category c = new Category();
+//        c.setName("新增加的Category");
+//        session.insert("addCategory",c);
 
-        List<Category> cs=session.selectList("listCategory");
+        //删
+//        Category c = new Category();
+//        c.setId(2);
+//        session.delete("deleteCategory",c);
+
+        //查
+//        Category c= session.selectOne("getCategory",3);
+//        System.out.println(c.getName());
+
+        //改
+//        Category c= session.selectOne("getCategory",3);
+//        c.setName("修改了的Category名称");
+//        session.update("updateCategory",c);
+
+        //查询所有
+        listAll(session);
+
+        session.commit();
+        session.close();
+
+
+    }
+    private static void listAll(SqlSession session) {
+        List<Category> cs = session.selectList("listCategory");
         for (Category c : cs) {
             System.out.println(c.getName());
         }
-
     }
 }
