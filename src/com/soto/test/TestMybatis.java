9273a23d12e71d6febb33016323b12e5e1bd370f
sotowang <sotowang@qq.com>
2018-07-13 14:04:54 +0800

    if语句

--- src/com/soto/test/TestMybatis.java
+++ src/com/soto/test/TestMybatis.java
@@ -78,12 +78,26 @@ public class TestMybatis {
 
 
         //删除关系
-        deleteOrderItem(session);
+//        deleteOrderItem(session);
 
         //多对多
-        listOrder(session);
+//        listOrder(session);
 
 
+        //if语句
+        System.out.println("查询所有的");
+        List<Product> ps = session.selectList("listProduct");
+        for (Product p : ps) {
+            System.out.println(p);
+        }
+
+        System.out.println("模糊查询");
+        Map<String,Object> params = new HashMap<>();
+        params.put("name","a");
+        List<Product> ps2 = session.selectList("listProduct",params);
+        for (Product p : ps2) {
+            System.out.println(p);
+        }
 
 
 
