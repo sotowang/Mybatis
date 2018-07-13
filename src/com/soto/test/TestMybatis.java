cdd1836f2efb440a7e15f94f6a47e85cf6d2ad30
sotowang <sotowang@qq.com>
2018-07-13 14:09:12 +0800

    where语句

--- src/com/soto/test/TestMybatis.java
+++ src/com/soto/test/TestMybatis.java
@@ -85,20 +85,29 @@ public class TestMybatis {
 
 
         //if语句
-        System.out.println("查询所有的");
-        List<Product> ps = session.selectList("listProduct");
-        for (Product p : ps) {
-            System.out.println(p);
-        }
+//        System.out.println("查询所有的");
+//        List<Product> ps = session.selectList("listProduct");
+//        for (Product p : ps) {
+//            System.out.println(p);
+//        }
+//
+//        System.out.println("模糊查询");
+//        Map<String,Object> params = new HashMap<>();
+//        params.put("name","a");
+//        List<Product> ps2 = session.selectList("listProduct",params);
+//        for (Product p : ps2) {
+//            System.out.println(p);
+//        }
 
-        System.out.println("模糊查询");
+        //where语句
+        System.out.println("多条件查询");
         Map<String,Object> params = new HashMap<>();
-        params.put("name","a");
+//        params.put("name","a");
+        params.put("price","10");
         List<Product> ps2 = session.selectList("listProduct",params);
         for (Product p : ps2) {
             System.out.println(p);
         }
-
 
 
         session.commit();
